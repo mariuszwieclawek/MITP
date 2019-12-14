344f77e825afc634e5dc68a26a93b3400d36ae8d
root <root@debian.mariusz>
2019-12-14 14:33:40 +0100

    TAK

--- zad16b.c
+++ zad16b.c
@@ -3,24 +3,19 @@
 
 void insertsort(int tab[], int x){
      for(int i = 0 ;i < x ; i++ ) {
-    /*storing current element whose left side is checked for its
-             correct position .*/
-      int temp = tab[ i ];
-      int j = i;
-       /* check whether the adjacent element in left side is greater or
-            less than the current element. */
-          while(  j > 0  && temp < tab[ j -1]) {
-           // moving the left side element to one position forward.
-                tab[ j ] = tab[ j-1];
-                j= j - 1;
+     	int temp = tab[ i ];
+     	int j = i;
+        	while(  j > 0  && temp < tab[ j -1]){
+                	tab[ j ] = tab[ j-1];
+                	j= j - 1;
            }
-         // moving current element to its  correct position.
            tab[ j ] = temp;
      }
-     printf("Posortowana tablica:");
+     printf("Posortowana tablica:{");
      for(int i=0; i<x; i++){
-        printf("%d ",tab[i]);
+        printf("%d",tab[i]);
      }
+     printf("}\n");
 }
 
 int main()
